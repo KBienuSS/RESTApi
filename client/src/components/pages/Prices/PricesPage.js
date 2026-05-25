2aa50558bba5748062973fbc4b8467319260e9a6
Kamil <bienius.234@gmail.com>
2026-05-26 00:20:47 +0200

    Add functionality in PricesPage

--- client/src/components/pages/Prices/PricesPage.js
+++ client/src/components/pages/Prices/PricesPage.js
@@ -1,24 +1,39 @@
-import { Alert, Container } from 'reactstrap';
+import { Alert, Progress, Container } from 'reactstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { getConcerts, getRequest, loadConcertsRequest } from '../../../redux/concertsRedux';
+import { useEffect } from 'react';
 
-const Prices = () => (
-  <Container>
-    <h1>Prices</h1>
-    <p>Prices may differ according the day of the festival. Remember that ticket includes not only the star performance, but also 10+ workshops. We gathered several genre teachers to help you increase your vocal skills, as well as self confidence.</p>
-    
-    <Alert color="info">
+const Prices = () => {
+
+  const dispatch = useDispatch();
+  const concerts = useSelector(getConcerts);
+  const request = useSelector(getRequest);
+
+  useEffect(() => {
+    dispatch(loadConcertsRequest());
+  }, [dispatch]);
+
+  if(request.pending) return <Progress animated color="primary" value={50} />;
+  else if(request.error) return <Alert color="warning">{request.error}</Alert>;
+  else if(!request.success || !concerts.length) return <Alert color="info">No concerts</Alert>;
+  else if(request.success) return (
+    <Container>
+      <h1>Prices</h1>
+      <p>Prices may differ according the day of the festival. Remember that ticket includes not only the star performance, but also 10+ workshops. We gathered several genre teachers to help you increase your vocal skills, as well as self confidence.</p>
+
+      <Alert color="info">
         Attention! <strong>Children under 4 can go freely with you without any other fee!</strong>
-    </Alert>
+      </Alert>
 
-    <h2>Day one</h2>
-    <p>Price: 25$</p>
-    <p>Workshops: "Rock Music Style", "How to make you voice grooowl", "Make your voice stronger", "History of Rock"</p>
-    <h2>Day Two</h2>
-    <p>Price: 25$</p>
-    <p>Workshops: "Find your real tune", "Find your real YOU", "Fell the music", "Jam session"</p>
-    <h2>Day three</h2>
-    <p>Price: 50$</p>
-    <p>Workshops: "Increase your vocal range", "How to properly warmup before singing", "It's time for YOU!"</p>
-  </Container>
-);
+      {concerts.map(concert => (
+        <div key={concert._id}>
+          <h2>Day {concert.day} – {concert.performer}</h2>
+          <p>Price: {concert.price}$</p>
+          <p>Genre: {concert.genre}</p>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 export default Prices;
